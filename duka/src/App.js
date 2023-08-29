95a84d5864ce7f7a004787e8bcbcd3900562a5c7
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-08-29 15:21:41 +0300

    James/development (#9)
    * feat:homepage updates
    
    * feat:homepage updates
    
    * feat:homepage updates
    
    * feat:homepage updates
    
    * feat: db fetch and display

--- duka/src/App.js
+++ duka/src/App.js
@@ -1,10 +1,19 @@
+import Home from "./components/Homepage";
 import Login from "./components/Login";
 
+
 function App() {
+
   return (
     <div className="App">
+      <nav>
+          <li>Home</li>
+          <li>Login</li>
+          <li>Cart</li>
+          <li>My Orders</li>
+      </nav>
       <Login/>
-      
+      <Home/>
     </div>
   );
 }
