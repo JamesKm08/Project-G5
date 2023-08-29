1fa9ac4d00689a90c55e32ce8417708eb394e774
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-08-29 15:07:28 +0300

    feat: db fetch and display

--- duka/src/App.js
+++ duka/src/App.js
@@ -1,8 +1,16 @@
+import Home from "./components/Homepage";
 
 function App() {
+
   return (
     <div className="App">
-      
+      <nav>
+          <li>Home</li>
+          <li>Login</li>
+          <li>Cart</li>
+          <li>My Orders</li>
+      </nav>
+      <Home/>
     </div>
   );
 }
--- duka/src/components/Homepage.js
+++ duka/src/components/Homepage.js
@@ -1,8 +1,27 @@
+import { useState,useEffect } from "react";
 
 function Home() {
+
+  const [products, setProducts] = useState([])
+    
+  useEffect(() =>{
+      fetch('http://localhost:8000/products')
+      .then(res => res.json())
+      .then(data => setProducts(data),[])
+  })
+
     return (
       <div className="Home">
-        <nav>Home </nav>
+        {products.map(product=>(
+          <div key= {product.id}>
+          <img src = {product.images[2]} alt={product.title}/>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <p>{product.price} Ksh</p>
+          </div>
+          
+      
+      ))}
       </div>
     );
   }
