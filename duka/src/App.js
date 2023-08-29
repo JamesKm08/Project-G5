71a37ce7e36df755f2ff1de9e58fb70ce963cc5c
Njokimary <marynjoki738@gmail.com>
2023-08-29 23:23:12 +0300

    feat:call the register file in app.js

--- duka/src/App.js
+++ duka/src/App.js
@@ -1,9 +1,12 @@
 import Login from "./components/Login";
+import Register from "./components/Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <Register/>
       
     </div>
   );
