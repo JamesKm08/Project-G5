cc6764fd304347df87726cb86bec80f22a189a75
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-08-31 11:15:34 +0300

    feat:homepage updates

--- duka/src/components/Homepage.js
+++ duka/src/components/Homepage.js
@@ -1,49 +1,52 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import './Homepage.css'
 
 function Home() {
 
-  const [products, setProducts] = useState([])
-    
+  const [products, setProducts] = useState([]);
+  const [hoveredProductId, setHoveredProductId] = useState(null); // Track hovered product ID
+
   useEffect(() => {
     fetch("http://localhost:8000/products")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
-console.log(products)
-    return (
-      <div className="Home">
-        {products.map(product=>(
-            <div className= "productdiv" key= {product.id}>
 
-          
-            <div>
-              <h3 className="titlediv">{product.title}</h3>
-            </div>
-
-            <div>
-              <img  className="imagediv"
-              src = {product.images[1]} alt={product.title}/>
-            </div>
-
-            {/* <div>
-              <p className="descdiv">{product.description}</p>
-              </div> */}
-          
-            <div>
-              <h3 className="pricediv">{product.price} USD</h3>
-            </div>
-
-            <div>
-                <button className="cartButton">Add to Cart</button>
-            </div>
-
+  return (
+    <div className="Home">
+      {products.map(product => (
+        <div
+          className="productdiv"
+          key={product.id}
+          onMouseEnter={() => setHoveredProductId(product.id)}
+          onMouseLeave={() => setHoveredProductId(null)}
+        >
+          <div>
+            <h3 className="titlediv">{product.title}</h3>
           </div>
-         
+
+          <div>
+            <img className="imagediv" src={product.images[1]} alt={product.title} height= "200px"/>
+          </div>
+
+          {hoveredProductId === product.id && (
+            <>
+            <div>
+              <p className="descdiv">{product.description}</p>
+            </div>
+          
+          <div>
+            <h3 className="pricediv">{product.price} USD</h3>
+          </div>
+          </>)}
+
+          <div>
+            <button className="cartButton">Add to Cart</button>
+          </div>
+        </div>
       ))}
-      </div>
-    );
-  }
-    
-  export default Home;
-  
+    </div>
+  );
+}
+
+export default Home;
