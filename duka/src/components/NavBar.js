31a585bcc3641b578a47017259960ca6c469953f
Njokimary <marynjoki738@gmail.com>
2023-08-31 15:33:20 +0300

    align items

--- duka/src/components/NavBar.js
+++ duka/src/components/NavBar.js
@@ -1,36 +1,33 @@
-import {  Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import './Navbar.css';
 
 const NavBar = () => {
-return(<>
-    <nav>
-    <Link to = "/">
-         <h4>Home</h4>
-        </Link>
-        
-        <Link to = "/Login">
-        <h4>Login</h4>
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/">
+          <h4>Home</h4>
         </Link>
 
-        <Link to = "/Contact">
-        <h4>Contact</h4>
+        <Link to="/Login">
+          <h4>Login</h4>
         </Link>
 
-        <Link to = "/Cart">
-        <h4>Cart</h4>
+        <Link to="/Contact">
+          <h4>Contact</h4>
         </Link>
 
-        <Link to = "/Register">
-        <h4>Register</h4>
+        <Link to="/Cart">
+          <h4>Cart</h4>
         </Link>
-       
-    </nav>
+
+        <Link to="/Register">
+          <h4>Register</h4>
+        </Link>
+      </nav>
     </>
-    
-)
+  );
+};
 
-}
 export default NavBar;
-
-
-
-          
