1080f78e1b158a0a443fa0816c71dbf49ca6025d
Njokimary <marynjoki738@gmail.com>
2023-09-04 11:07:33 +0300

    feat:calculate subtotal of items on cart

--- duka/src/components/Cart.js
+++ duka/src/components/Cart.js
@@ -1,32 +1,73 @@
-import React, { useState } from 'react';
-import '../styles/cart.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import CartItem from './CartItem';
 
-function Cart({ cartItems, removeFromCart }) {
-  const [removedItem, setRemovedItem] = useState(null);
+const Cart = ({ cartItems, onRemoveFromCart, onEmptyCart, onUpdateCartQty }) => {
+  const handleEmptyCart = () => {
+    onEmptyCart();
+  };
 
-  const handleRemoveFromCart = (itemId) => {
-    removeFromCart(itemId);
-    setRemovedItem(itemId);
-    setTimeout(() => {
-      setRemovedItem(null);
-    }, 4000); // Hide the notification after 2 seconds
+  const renderEmptyMessage = () => {
+    if (!cartItems || cartItems.length === 0) {
+      return (
+        <p className="cart__none">
+          You have no items in your shopping cart, start adding some!
+        </p>
+      );
+    }
+    return null;
+  };
+  const renderItems = () => {
+    return cartItems.map((item, index) => (
+      <CartItem
+        item={item}
+        onUpdateCartQty={onUpdateCartQty}
+        onRemoveFromCart={onRemoveFromCart}
+        key={`${item.id}-${index}`}
+      />
+    ));
+  };
+  
+
+  const renderTotal = () => {
+    if (!cartItems || cartItems.length === 0) {
+      return null;
+    }
+
+    const total = cartItems.reduce((acc, item) => {
+      if (item.line_total && item.line_total.raw) {
+        return acc + item.line_total.raw;
+      }
+      return acc;
+    }, 0);
+
+    return (
+      <div className="cart__total">
+        <p className="cart__total-title">Subtotal:</p>
+        <p className="cart__total-price">{total} USD</p>
+      </div>
+    );
   };
 
   return (
     <div className="cart">
-      <h2>Cart</h2>
-      <ul>
-        {cartItems.map(item => (
-          <li key={item.id}>
-            {item.title} - ${item.price}
-            <button onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-      {removedItem && <p>Product removed from cart</p>}
+      <h4 className="cart__heading">Your Shopping Cart</h4>
+      {renderEmptyMessage()}
+      {renderItems()}
+      {renderTotal()}
+      <div className="cart__footer">
+        <button className="cart__btn-empty" onClick={handleEmptyCart}>Empty cart</button>
+        <button className="cart__btn-checkout">Checkout</button>
+      </div>
     </div>
   );
-}
+};
 
 export default Cart;
 
+Cart.propTypes = {
+  cartItems: PropTypes.array,
+  onRemoveFromCart: PropTypes.func,
+  onUpdateCartQty: PropTypes.func,
+  onEmptyCart: PropTypes.func,
+};
